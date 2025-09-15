5830b65f9f491c5558e386deb801e5f74be00f9d
vanish <78757142+Vanish0314@users.noreply.github.com>
2025-09-15 22:12:05 +0800

    Enhance Program.cs with Observable.Range and SubscribeOn examples
    - Added an Observable.Range subscription to demonstrate synchronous execution on the main thread.
    - Included a comment explaining the potential blocking behavior when not using SubscribeOn.
    - Introduced an Observable.Interval subscription with SubscribeOn to allow the main thread to continue executing while processing ticks on a separate thread.
    - Updated console output to reflect the new subscription behavior and thread details.

--- Program.cs
+++ Program.cs
@@ -3,6 +3,14 @@ using System.Reactive.Linq;
 
 Console.WriteLine($"[T:{Environment.CurrentManagedThreadId}] Main thread");
 
+// 不使用SubscribeOn,那么这里的Subscribe会在主线程执行,导致如果是Interval的话,那么程序就会卡这里不动了.
+Observable
+    .Range(1, 5)
+    .Subscribe(tick => Console.WriteLine($"[T:{Environment.CurrentManagedThreadId}] Tick {tick}"));
+
+Console.WriteLine("Range Done");
+
+// 使用SubscribeOn到别地线程进行订阅,这样主线程继续执行.
 Observable
     .Interval(TimeSpan.FromSeconds(1))
     .SubscribeOn(
