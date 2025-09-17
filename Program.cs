d59296a2766cb7e1c726c83f850eef395f6abea5
vanish <78757142+Vanish0314@users.noreply.github.com>
2025-09-17 20:47:18 +0800

    OvervalOn vs. pass ISchedduler

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+
+var subscription = Observable
+    .Interval(TimeSpan.FromSeconds(1), ImmediateScheduler.Instance)
+    .SubscribeOn(new EventLoopScheduler())
+    .Subscribe(Console.WriteLine);
+
+Console.WriteLine("Program Ended");
+
+var subscription0 = Observable
+    .Interval(TimeSpan.FromSeconds(1), ImmediateScheduler.Instance)
+    .Subscribe(Console.WriteLine);
+
+Console.WriteLine("Program REALLY Ended");
