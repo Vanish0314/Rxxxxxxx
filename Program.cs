746ea05e646f61cc634126bd8263516056db682f
vanish <78757142+Vanish0314@users.noreply.github.com>
2025-09-17 20:54:20 +0800

    传入调度器与SubscribeOn的比较的最屌的例子

--- Program.cs
+++ Program.cs
@@ -2,15 +2,72 @@
 using System.Reactive.Concurrency;
 using System.Reactive.Linq;
 
-var subscription = Observable
-    .Interval(TimeSpan.FromSeconds(1), ImmediateScheduler.Instance)
-    .SubscribeOn(new EventLoopScheduler())
-    .Subscribe(Console.WriteLine);
+public class MainThreadScheduler : IScheduler
+{
+    private readonly int _mainThreadId;
 
-Console.WriteLine("Program Ended");
+    public MainThreadScheduler()
+    {
+        _mainThreadId = Environment.CurrentManagedThreadId;
+    }
 
-var subscription0 = Observable
-    .Interval(TimeSpan.FromSeconds(1), ImmediateScheduler.Instance)
-    .Subscribe(Console.WriteLine);
+    public DateTimeOffset Now => DateTimeOffset.Now;
 
-Console.WriteLine("Program REALLY Ended");
+    public IDisposable Schedule<TState>(TState state, Func<IScheduler, TState, IDisposable> action)
+    {
+        if (Environment.CurrentManagedThreadId != _mainThreadId)
+            throw new InvalidOperationException("只能在主线程上调度");
+
+        return action(this, state);
+    }
+
+    public IDisposable Schedule<TState>(
+        TState state,
+        TimeSpan dueTime,
+        Func<IScheduler, TState, IDisposable> action
+    )
+    {
+        if (dueTime <= TimeSpan.Zero)
+        {
+            return Schedule(state, action);
+        }
+
+        Thread.Sleep(dueTime);
+        return Schedule(state, action);
+    }
+
+    public IDisposable Schedule<TState>(
+        TState state,
+        DateTimeOffset dueTime,
+        Func<IScheduler, TState, IDisposable> action
+    )
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var subscription = Observable
+            .Interval(TimeSpan.FromSeconds(1), ImmediateScheduler.Instance)
+            .SubscribeOn(new EventLoopScheduler())
+            .Subscribe(Console.WriteLine);
+
+        Console.WriteLine("Program Ended 0");
+
+        var subscription0 = Observable
+            .Interval(TimeSpan.FromSeconds(1), new MainThreadScheduler())
+            .SubscribeOn(new EventLoopScheduler())
+            .Subscribe(Console.WriteLine);
+
+        Console.WriteLine("Program Ended 1");
+
+        var subscription1 = Observable
+            .Interval(TimeSpan.FromSeconds(1), ImmediateScheduler.Instance)
+            .Subscribe(Console.WriteLine);
+
+        Console.WriteLine("Program REALLY Ended");
+    }
+}
